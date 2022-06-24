212495513c1a825b425b7e11ad90045b94d86226
mautin <nunayonkk@gmail.com>
2022-06-24 18:20:10 +0100

    my 1st commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let myName = ["Amosu Mautin"];
+let myHeigth = "Heigth" +  5.5;
+let myCountry = "Nationality"+   "Nigeria";
+document.getElementById("demo").innerHTML =
+  myName + "<br>" + myHeigth + "<br>" + myCountry;
